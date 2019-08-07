3aa947867de3125cac90c94e403cd8b68668398d
Amit Shinde <shinde13amit@gmail.com>
2019-08-08 03:21:04 +0530

    feat: update icon and api url

--- employee-review-ui/src/services/employee.service.js
+++ employee-review-ui/src/services/employee.service.js
@@ -18,7 +18,7 @@ import {
 
 import http from "./Http.service";
 
-const API_URL = "http://localhost:9000/api";
+const API_URL = `${window.location.href}/api`;
 const API_VERSION = "/v1";
 const EMPLOYEES = "/employees";
 const EMPLOYEES_URL = API_URL + API_VERSION + EMPLOYEES;
